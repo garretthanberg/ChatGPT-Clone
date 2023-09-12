4f565ef5c5bb4f862eef102521540f3e84a493ca
Garrett Hanberg <115447682+garretthanberg@users.noreply.github.com>
2023-09-11 17:40:08 -0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,12 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKm_DXO7d-waPNcJuGa3kqHFuy7Bwgz9w",
-  authDomain: "chatgpt-clone-gh.firebaseapp.com",
-  projectId: "chatgpt-clone-gh",
-  storageBucket: "chatgpt-clone-gh.appspot.com",
-  messagingSenderId: "784255864800",
-  appId: "1:784255864800:web:096c99da65f40056a26585",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
